d7b465ac117350acf41a1034c2b1a8085a686ad2
Teri Pastorino <tpasto@comcast.net>
2014-10-25 21:50:49 -0700

    updated production rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,15 @@ Rails.application.configure do
   #required for heroku
   #note to set this to actual host name 'omr-teripastorino.com'
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-end
-  #sets papercilip to upload images to s3
+
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+end
+  #sets papercilip to upload images to s3
+  
     }
   }
